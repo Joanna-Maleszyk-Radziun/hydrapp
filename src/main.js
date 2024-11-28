99bc8fab21e3b9e3a1ec7ff5c0a1820d162ba5f3
Joanna-Maleszyk-Radziun <artmaleszyk@gmail.com>
2024-11-28 15:47:52 +0100

    fixed letters

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 const addButton = document.querySelector('.add--js');
 const subtractButton = document.querySelector('.subtract--js');
 const counterValue = document.querySelector('.counter--js');
-
+alert('test')
 let glasses = 0;
 
 let key = new Date().toLocaleString().slice(0,10);
 
 setInterval (() => {
     console.log('tik-tak')
-    if(key !== new Date().toLocaleString().slice(0,10)) {
+    if (key !== new Date().toLocaleString().slice(0,10)) {
         key = new Date().toLocaleString().slice(0,10)
         setCounterValue(0)
         glasses = 0
